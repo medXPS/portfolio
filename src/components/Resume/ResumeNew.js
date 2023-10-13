b3c43539498fe00f4c1aa863ad28a42d0bb7cb21
medXPS <mohamedammaha2020@gmail.com>
2023-10-13 01:38:54 +0100

    adding  new cv

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Cv_mohamed_AMMAHA .pdf";
+import pdf from "../../Assets/../Assets/Cv_mohamed_AMMAHA_v2 .pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
